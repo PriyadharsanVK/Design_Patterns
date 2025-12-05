7c82ad3ff440c2f0dcdf3938c9586ac354dcc906
PriyadharsanVK <vkpriyadharsan@gmail.com>
2025-12-05 13:01:11 +0530

    Second Commit

---
+++ CompositeDesignPattern/src/HeadDepartment.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeadDepartment implements Department{
+    private Integer id;
+    private String name;
+    private List<Department> childDepartment;
+
+    public HeadDepartment(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+        this.childDepartment = new ArrayList<>();
+    }
+
+    @Override
+    public void printDepartmentName() {
+        childDepartment.forEach(Department::printDepartmentName);
+    }
+
+    public void addDepartment(Department department){
+        childDepartment.add(department);
+    }
+
+    public void removeDepartment(Department department){
+        childDepartment.remove(department);
+    }
+}
